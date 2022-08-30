e196c44212acd67bc2e209c4387772a39785aa3b
Mahmoud Mohamed <mm8871918@gmail.com>
2022-08-30 06:12:08 +0200

    Uplaod the project

---
+++ src/test/java/guru/springframework/sf_pet_clinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sf_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
